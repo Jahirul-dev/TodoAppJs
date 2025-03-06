d101311959403258c9c70f898fdf458ddc2b6af3
jitu <jahirul.jitu7@gmail.com>
2025-03-06 18:04:05 +0600

    ver add.1.1.2

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let addForm = document.querySelector(".add");
 let getUl = document.querySelector("ul");
 let getULinner = getUl.innerHTML;
+let createLI = document.createElement("tr");
 
 addForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -8,4 +9,11 @@ addForm.addEventListener("submit", (e) => {
   console.log(valOfIn);
 });
 
-console.log(getULinner[1]);
+//console.log(getULinner);
+
+createLI.innerHTML = `<li
+            class="list-group-item d-flex justify-content-between align-items-center"
+          >
+            <span>play mariokart</span>
+            <i class="far fa-trash-alt delete"></i>
+          </li>`;
