685a3b6b5486a83bd76ea21b40b37aff0e54684d
jitu <jahirul.jitu7@gmail.com>
2025-03-06 17:52:17 +0600

    ver add.1.1

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let addForm = document.querySelector(".add");
+let getUl = document.querySelector("ul");
+let getULinner = getUl.innerHTML;
 
-addForm.addEventListener('submit', e=>{
-    e.preventDefault(); 
-    let valOfIn = document.querySelector('.addForm').value;
-    console.log(valOfIn);
-    
-})
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let valOfIn = document.querySelector(".addForm").value;
+  console.log(valOfIn);
+});
+
+console.log(getULinner[1]);
