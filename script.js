14c231b04d06ea3e291283fc9772e00be20a4960
Jahirul-dev <jahirul.jitu7@gmail.com>
2025-03-18 07:16:09 +0600

    li added

--- script.js
+++ script.js
@@ -1,20 +1,58 @@
-let addForm = document.querySelector(".add");
-let getUl = document.querySelector("ul");
-let getULinner = getUl.innerHTML;
-let createLI = document.createElement("tr");
+let getForm = document.getElementById("add");
+let getField = document.querySelector('.addForm');
+let createNewiL = document.querySelector('.todos')
 
-addForm.addEventListener("submit", (e) => {
-  // e.preventDefault();
-  let valOfIn = document.querySelector(".addForm").value;
-  console.log(valOfIn);
-
-  createLI.innerHTML = `<li
+function genarator(val){
+    let htmlCreate = `
+          <li
             class="list-group-item d-flex justify-content-between align-items-center"
           >
-            <span>play mariokart</span>
+            <span>${val}</span>
             <i class="far fa-trash-alt delete"></i>
-          </li>`;
-  getUl.append(createLI);
-});
+          </li>
+    `
+
+    createNewiL.innerHTML += htmlCreate
+
+    
+    
+}
+
+getForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    let formVal = getForm.add.value
+    onclick = getField.value = " "
+    console.log(formVal);
+
+    genarator(formVal)
+    
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// addForm.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   let valOfIn = document.querySelector(".addForm").value;
+//   console.log(valOfIn);
+
+//   createLI.innerHTML = `<li
+//             class="list-group-item d-flex justify-content-between align-items-center"
+//           >
+//             <span>play mariokart</span>
+//             <i class="far fa-trash-alt delete"></i>
+//           </li>`;
+//   getUl.append(createLI);
+// });
 
 //console.log(getULinner);
+
